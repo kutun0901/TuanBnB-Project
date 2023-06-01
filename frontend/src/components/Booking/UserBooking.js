7aa4d7105555f9112b014403c2604b9cc23a221a
kutun0901 <tuan.tq2433@gmail.com>
2023-06-01 09:55:11 -0700

    change delete booking button to modal

--- frontend/src/components/Booking/UserBooking.js
+++ frontend/src/components/Booking/UserBooking.js
@@ -2,46 +2,54 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserBookingsThunk } from "../../store/booking";
 import { Link } from "react-router-dom";
-import "./UserBooking.css"
+import "./UserBooking.css";
+import OpenModalButton from "../OpenModalButton";
 
 function UserBookings() {
-    const dispatch = useDispatch();
-    const userBookings = Object.values(useSelector(state => state.bookings.userBookings)).reverse();
+  const dispatch = useDispatch();
+  const userBookings = Object.values(
+    useSelector((state) => state.bookings.userBookings)
+  ).reverse();
 
-    useEffect(() => {
-        dispatch(getUserBookingsThunk())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getUserBookingsThunk());
+  }, [dispatch]);
 
-    return (
-        <>
-            <h1>My bookings</h1>
-            <div className="my-booking-container">
-                {userBookings.map(booking => {
-                    return (
-                        <div className="single-booking" key={booking.id}>
-                            <Link to={`/spots/${booking.spotId}`}>
-                                <div>
-                                    <img src={booking.Spot.previewImage} alt="spot-review-img" />
-                                </div>
-                            </Link>
-                            <div className="booking-info">
-                                <h3>{booking.Spot.name}</h3>
-                                <p>{booking.Spot.address}, {booking.Spot.city}, {booking.Spot.state}</p>
-                                <p>{booking.Spot.country}</p>
-                                <p>Check-in: {booking.startDate.slice(0, 10)}</p>
-                                <p>Checkout: {booking.endDate.slice(0, 10)}</p>
-                                <div className="booking-button-container">
-                                    <span><button>Edit</button></span>
-                                    <span><button>Delete</button></span>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
-
+  return (
+    <>
+      <h1>My bookings</h1>
+      <div className="my-booking-container">
+        {userBookings.map((booking) => {
+          return (
+            <div className="single-booking" key={booking.id}>
+              <Link to={`/spots/${booking.spotId}`}>
+                <div>
+                  <img src={booking.Spot.previewImage} alt="spot-review-img" />
+                </div>
+              </Link>
+              <div className="booking-info">
+                <h3>{booking.Spot.name}</h3>
+                <p>
+                  {booking.Spot.address}, {booking.Spot.city},{" "}
+                  {booking.Spot.state}
+                </p>
+                <p>{booking.Spot.country}</p>
+                <p>Check-in: {booking.startDate.slice(0, 10)}</p>
+                <p>Checkout: {booking.endDate.slice(0, 10)}</p>
+                <div className="booking-button-container">
+                  <button>Edit</button>
+                  <button>
+                    <OpenModalButton modalComponent={} buttonText="Cancel">
+                    Delete
+                    </button>
+                </div>
+              </div>
             </div>
-        </>
-    )
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default UserBookings;
