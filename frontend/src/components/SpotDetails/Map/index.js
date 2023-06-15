b7bf9f4639db926ffa6b4fcef49a0a9f3c078243
kutun0901 <tuan.tq2433@gmail.com>
2023-06-15 11:10:00 -0700

    add infoWindow displaying exact location provided after booking

--- frontend/src/components/SpotDetails/Map/index.js
+++ frontend/src/components/SpotDetails/Map/index.js
@@ -1,21 +1,29 @@
-import {GoogleMap, useJsApiLoader, Marker} from "@react-google-maps/api"
-import { useMemo } from "react"
-import "./Map.css"
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from "@react-google-maps/api";
+import { useMemo, useState } from "react";
+import "./Map.css";
 
 export default function Map({ spot }) {
-    // const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
 
-    const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    });
+  const center = useMemo(() => ({ lat: Number(spot.lat), lng: Number(spot.lng) }), [spot]);
+  const [infoWindowOpen, setInfoWindowOpen] = useState(false);
 
-    const center = useMemo(() => ({lat: Number(spot.lat), lng: Number(spot.lng)}), [spot])
+  const handleMarkerClick = () => {
+    setInfoWindowOpen(!infoWindowOpen);
+  };
 
-    if (!isLoaded) return <div>Loading...</div>
+  if (!isLoaded) return <div>Loading...</div>;
 
-    return (
-        <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-            <Marker position={center} />
-        </GoogleMap>
-    )
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <Marker position={center} onClick={handleMarkerClick} />
+      {infoWindowOpen && (
+        <InfoWindow position={center} onCloseClick={handleMarkerClick}>
+          <div>Exact location provided after booking</div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
 }
